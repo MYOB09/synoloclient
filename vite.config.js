a017dc68a0c24273398f5f626c522963f73c5317
Argyris <vassilopargiris@gmail.com>
2026-01-18 03:17:57 +0200

    removed forgoten / from images again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/synoloclient/',
+  base: '/synoloclient/',//otan paroume domain prepei na schstei afth h grammh
   plugins: [react()],
 })
